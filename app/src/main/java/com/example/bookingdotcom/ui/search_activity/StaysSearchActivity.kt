581d15681c65e45b479dc1ce593718007723b2f9
pctien <pctien>
2023-08-08 00:42:08 +0700

    fragments ui in search fragment

---
+++ app/src/main/java/com/example/bookingdotcom/ui/search_activity/StaysSearchActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bookingdotcom.ui.search_activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.R
+
+class StaysSearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stays_search)
+    }
+}
